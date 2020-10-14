4c534378368e1b249b2318f012651d457c94c366
Steven Crosby <65566479+scrosby09@users.noreply.github.com>
2020-10-13 21:53:46 -0700

    commit

--- lesson 04/js/main.js
+++ lesson 04/js/main.js
@@ -1,18 +1,18 @@
 document.getElementById("modified").innerHTML = todayDate();
 
 function todayDate() {
-    var today = new Date();
+    let today = new Date();
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const months = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
-    var month = months[today.getMonth()];
-    var day = days[today.getDay()];
-    var dayDate = today.getDate();
-    var year = today.getFullYear();
+    let month = months[today.getMonth()];
+    let day = days[today.getDay()];
+    let dayDate = today.getDate();
+    let year = today.getFullYear();
     return  day + ", " + dayDate + " " + month + " " + year;
 }
 
 function toggleMenu ()  {
-    if (document.getElementsByClassName("navigation")[0].classList == "navigation"){
+    if (document.getElementsByClassName("navigation")[0].classList === "navigation"){
         document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
         document.getElementById("ham").innerHTML= "&#x2715";
     } else {
