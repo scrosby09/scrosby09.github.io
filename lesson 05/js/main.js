c1b26ec76c6fd2bc81561827175a6d10c43636c4
Steven Crosby <65566479+scrosby09@users.noreply.github.com>
2020-10-23 13:38:04 -0700

    commit

--- lesson 05/js/main.js
+++ lesson 05/js/main.js
@@ -38,9 +38,31 @@ document.getElementById("updated").textContent = fulldate;
 //Pancake Display Banner 
 
 const banner = document.getElementById('banner');
-if (d.getDay()==5){
+if (d.getDay() == 5) {
     banner.style.display = 'block';
 }
 else {
-    banner.style.display='none';
+    banner.style.display = 'none';
+}
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function date() {
+    var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var d = new Date();
+    var weekday = d.getDay();
+    var dayOfMonth = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+
+    var fullDate = weekdays[weekday] + ', ' + dayOfMonth + " " + months[month] + " " + year;
+    document.getElementById("modified").innerHTML = fullDate;
+
+    if (weekday === 5) {
+        document.getElementById("announcements").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+        document.getElementById("announcements").style.display = "block";
+    }
 }
