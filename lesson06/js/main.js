1376dbb2c635b75fe23cfc5a6bb1756c9fca7ae7
Steven Crosby <65566479+scrosby09@users.noreply.github.com>
2020-10-31 15:37:42 -0700

    lesson06 updates

--- lesson06/js/main.js
+++ lesson06/js/main.js
@@ -1,68 +1,38 @@
-//Toggle Menu Start
+/*Display date in the footer*/
+document.getElementById("modified").innerHTML = todayDate();
+function todayDate() {
+    var today = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var month = months[today.getMonth()];
+    var day = days[today.getDay()];
+    var dayDate = today.getDate();
+    var year = today.getFullYear();
+    return day + ", " + dayDate + " " + month + " " + year;
+}
+
+/*Hamburger menu for small display*/
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
-
-//Current Date Updated Start
-const daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-document.getElementById("updated").textContent = fulldate;
-
-
-//Pancake Display Banner 
-
-const banner = document.getElementById('banner');
-if (d.getDay() == 5) {
-    banner.style.display = 'block';
-}
-else {
-    banner.style.display = 'none';
-}
-
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("hide");
-}
-
-function date() {
-    var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    var d = new Date();
-    var weekday = d.getDay();
-    var dayOfMonth = d.getDate();
-    var month = d.getMonth();
-    var year = d.getFullYear();
-
-    var fullDate = weekdays[weekday] + ', ' + dayOfMonth + " " + months[month] + " " + year;
-    document.getElementById("modified").innerHTML = fullDate;
-
-    if (weekday === 5) {
-        document.getElementById("announcements").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-        document.getElementById("announcements").style.display = "block";
+    if (document.getElementsByClassName("navigation")[0].classList == "navigation") {
+        document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+        document.getElementById("ham").innerHTML = "&#x2715";
+    } else {
+        document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+        document.getElementById("ham").innerHTML = "&#9776 Menu";
     }
+
+}
+
+/*Banner at top of screen for Friday's*/
+document.getElementById("banner").innerHTML = banner();
+function banner() {
+    var today = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var day = days[today.getDay()];
+    if (day == "Friday") {
+        document.getElementById("banner").style.display = "flex";
+    } else {
+        document.getElementById("banner").style.display = "none";
+    }
+    return "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
 }
