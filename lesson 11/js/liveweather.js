1b72b0f91c6d2b7009d2abaf71fe838139dc58cf
steven-crosby <73408604+steven-crosby@users.noreply.github.com>
2020-12-08 15:29:02 -0800

    commit

--- lesson 11/js/liveweather.js
+++ lesson 11/js/liveweather.js
@@ -1,4 +1,4 @@
-const townIDs = [{
+const town_id = [{
     name: 'fish-haven',
     id: '5585010'
 }, {
@@ -9,63 +9,63 @@ const townIDs = [{
     id: '5607916'
 }];
 
-function townIDLookup() {
+function town_id_match() {
     const pathName = window.location.pathname;
-    for (let i = 0; i < townIDs.length; i++) {
-        let townName = townIDs[i].name;
+    for (let i = 0; i < town_id.length; i++) {
+        let townName = town_id[i].name;
         if (pathName.includes(townName)) {
-            return townIDs[i].id;
+            return town_id[i].id;
         }
     }
 }
 
-const townID = townIDLookup();
-const appID = "4a734b6bccba91cbab2bd77dba07fc5c";
-const unit = "imperial";
-const currentWeatherDataURL = "https://api.openweathermap.org/data/2.5/weather?id=" + townID + "&units=" + unit + "&APPID=" + appID;
-const forecastDataURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + townID + "&units=" + unit + "&APPID=" + appID;
+const town_match = town_id_match();
+const api_id = "4a734b6bccba91cbab2bd77dba07fc5c";
+const metric = "imperial";
+const weather_api_url = "https://api.openweathermap.org/data/2.5/weather?id=" + town_match + "&units=" + metric + "&APPID=" + api_id;
+const forecast_api_url = "https://api.openweathermap.org/data/2.5/forecast?id=" + town_match + "&units=" + metric + "&APPID=" + api_id;
 
-const windChillCalc = (t, s) => {
-    let windChill = 'N/A';
-    if (t <= 50 && s > 3.0) {
-        windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-        return Math.round(windChill) + "&deg;F";
+const windchill_calculator = (temp, windspeed) => {
+    let wind_chill = 'N/A';
+    if (temp <= 50 && windspeed > 3.0) {
+        wind_chill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temp * Math.pow(windspeed, 0.16);
+        return Math.round(wind_chill) + "&deg;F";
     } else {
-        return windChill;
+        return wind_chill;
     }
 }
 
-fetch(currentWeatherDataURL)
+fetch(weather_api_url)
     .then((response) => response.json())
-    .then((currentWeatherObject) => {
+    .then((current_weather) => {
 
-        //console.log(currentWeatherObject);
-        let h = currentWeatherObject.main.temp_max;
-        let l = currentWeatherObject.main.temp_min;
-        let t = currentWeatherObject.main.temp;
-        let s = currentWeatherObject.wind.speed;
-        document.getElementById("currentCondition").innerText = currentWeatherObject.weather[0].main;
-        document.getElementById("highTemp").innerHTML = Math.round(h) + "&deg;F / " + Math.round(l) + "&deg;F";
-        document.getElementById("currentTemp").innerHTML = Math.round(t) + "&deg;F";
-        document.getElementById("windChill").innerHTML = windChillCalc(t, s);
-        document.getElementById("humidity").innerHTML = currentWeatherObject.main.humidity + "&percnt;";
-        document.getElementById("windSpeed").innerText = Math.round(s) + "mph";
+        //console.log(current_weather);
+        let high_temp = current_weather.main.temp_max;
+        let low_temp = current_weather.main.temp_min;
+        let current_temp = current_weather.main.temp;
+        let wind_speed = current_weather.wind.speed;
+        document.getElementById("current-conditions").innerText = current_weather.weather[0].main;
+        document.getElementById("high-temp").innerHTML = Math.round(high_temp) + "&deg;F / " + Math.round(low_temp) + "&deg;F";
+        document.getElementById("current-temp").innerHTML = Math.round(current_temp) + "&deg;F";
+        document.getElementById("wind-chill").innerHTML = windchill_calculator(current_temp, wind_speed);
+        document.getElementById("humidity").innerHTML = current_weather.main.humidity + "&percnt;";
+        document.getElementById("wind-speed").innerText = Math.round(wind_speed) + "mph";
     });
 
-fetch(forecastDataURL)
+fetch(forecast_api_url)
     .then((response) => response.json())
-    .then((weatherForecastObject) => {
+    .then((weather_forecast) => {
 
-        //console.log(weatherForecastObject);
-        for (let i = 0; i < weatherForecastObject.list.length; i++) {
-            let dateCheck = weatherForecastObject.list[i].dt_txt;
-            if (dateCheck.includes("18:00:00")) {
+        //console.log(weather_forecast);
+        for (let i = 0; i < weather_forecast.list.length; i++) {
+            let check_date = weather_forecast.list[i].dt_txt;
+            if (check_date.includes("18:00:00")) {
                 //Create day wrapper
                 let forecast_day = document.createElement("div");
                 forecast_day.className = "forecast-day";
 
                 //Format and append Weekday
-                let full_date = new Date(weatherForecastObject.list[i].dt_txt);
+                let full_date = new Date(weather_forecast.list[i].dt_txt);
                 let date_selection = {
                     weekday: 'long'
                 };
@@ -81,20 +81,20 @@ fetch(forecastDataURL)
 
                 //Create and append image to weather detail wrapper
                 let weather_icon = document.createElement("img");
-                weather_icon.setAttribute('src', 'https://openweathermap.org/img/w/' + weatherForecastObject.list[i].weather[0].icon + '.png');
-                weather_icon.setAttribute('alt', weatherForecastObject.list[i].weather[0].description);
+                weather_icon.setAttribute('src', 'https://openweathermap.org/img/w/' + weather_forecast.list[i].weather[0].icon + '.png');
+                weather_icon.setAttribute('alt', weather_forecast.list[i].weather[0].description);
                 weather.appendChild(weather_icon);
 
                 //Create and append high/low to weather detail wrapper
                 let temp = document.createElement("p");
-                temp.innerHTML = Math.round(weatherForecastObject.list[i].main.temp) + "&deg;F";
+                temp.innerHTML = Math.round(weather_forecast.list[i].main.temp) + "&deg;F";
                 weather.appendChild(temp);
 
                 //Append weather info to day wrapper
                 forecast_day.appendChild(weather);
 
                 //Output new day in weather-forecast
-                document.getElementById("forecast-container").appendChild(forecast_day);
+                document.getElementById("fiveday-forecast").appendChild(forecast_day);
             }
         }
     });
